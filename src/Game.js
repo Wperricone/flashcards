07e29b7a59a4903a70ce16a117ae09bf22e10722
Whitney Perricone <whitney.perricone@gmail.com>
2022-03-24 14:30:28 -0600

    Finish Builting Game

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,43 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 
 class Game {
-  constructor() {}
-
-  printMessage(deck, round) {
+  constructor() {
+  this.cards = []
+}
+  printMessage(deck = this.deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
-  printQuestion(round) {
+  printQuestion(round = this.currentRound) {
       util.main(round);
   }
+
+  start() {
+    this.createCards();
+    this.createDeck();
+    this.createRound();
+    this.printMessage();
+    this.printQuestion();
+  }
+  createCards() {
+    prototypeQuestions.forEach((question) =>  {
+      this.cards.push(new Card(question.id, question.question, question.answers, question.correctAnswer))
+    })
+  }
+  createDeck() {
+    this.deck = new Deck(this.cards)
+  }
+  createRound() {
+    this.currentRound = new Round(this.deck);
+  }
+
 }
 
 module.exports = Game;
